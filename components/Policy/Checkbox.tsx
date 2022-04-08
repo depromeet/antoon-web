beb9a4037a12b348e9bc666a8ceae5cbe0db9960
tolluset <dlqud19@gmail.com>
2022-04-08 20:01:21 +0900

    feat: :sparkles: #10 add service policy agree checkbox

---
+++ components/Policy/Checkbox.tsx
@@ -0,0 +1,23 @@
+import { Children } from 'react';
+
+interface Props {
+    id: string;
+    link: string;
+    children: React.ReactNode;
+}
+
+const Checkbox = (props: Props) => {
+    const { id, link, children } = props;
+
+    return (
+        <>
+            <input type="checkbox" id={id}></input>
+            <label htmlFor={id}>{children}</label>
+            <a href={link} target="_blank" rel="noreferrer">
+                보기
+            </a>
+        </>
+    );
+};
+
+export default Checkbox;
