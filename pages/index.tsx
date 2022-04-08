1dfddd6cf2b1f2c6f8d8140da9333f5ed6402b68
Lee Byonghun <dlqud19@gmail.com>
2022-04-09 02:28:17 +0900

    ✨ [정책 동의] 체크박스 컴포넌트 추가 (#11)
    * chore: :wrench: modify semicolon rule from false to true
    
    * feat: :sparkles: #10 add service policy agree checkbox
    
    * feat: :construction: add temporary policy page
    
    * chore: :books: add policy checkbox stories
    
    * fix: :art: use react funtion component from expression to declaration
    
    * chore: :art: move story book files to near by original component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-    return <></>
-}
+    return <></>;
+};
 
-export default Home
+export default Home;
