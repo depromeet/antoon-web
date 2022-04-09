b087556523a2c74400124da3ae3e7d56bc230283
mnxmnz <codi0703@naver.com>
2022-04-09 13:18:56 +0900

    ⚙️ [chore] auth type 선언, meta tag 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { SessionProvider, useSession } from 'next-auth/react'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from '@emotion/react'
@@ -10,17 +10,19 @@ import { ThemeProvider } from '@emotion/react'
 import themes from '../styles/Theme/themes'
 import GlobalStyle from '../styles/GlobalStyle/GlobalStyle'
 
+interface AppPropsWithAuth extends AppProps {
+    Component: AppProps['Component'] & { auth: boolean }
+}
+
 const queryClient = new QueryClient()
 
-function MyApp({ session, Component, pageProps }: AppProps) {
+function MyApp({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppPropsWithAuth) {
     return (
         <>
             <Head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
                 <title>Depromeet 11th 1team</title>
             </Head>
             <SessionProvider session={session}>
@@ -44,7 +46,7 @@ function MyApp({ session, Component, pageProps }: AppProps) {
     )
 }
 
-function Auth({ children }) {
+function Auth({ children }: { children: React.ReactNode & JSX.Element }) {
     const { status } = useSession({ required: true })
 
     if (status === 'loading') {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head></Head>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
