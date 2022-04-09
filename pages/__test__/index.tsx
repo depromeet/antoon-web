be10660cf60a10e493cb42a098ba5ed45144f020
choipureum <pooreumsunny@gmail.com>
2022-04-09 17:02:08 +0900

    :fire:[feature]resolve conflict
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

--- pages/__test__/index.tsx
+++ pages/__test__/index.tsx
@@ -3,10 +3,10 @@ import Charts from '../../components/charts/Charts';
 import getChartOption from '@components/charts/composables/Charts_Options';
 
 const ChartTest: React.FC = () => {
-	return (
-		<div>
-			<Charts option={getChartOption('')} />
-		</div>
-	);
+  return (
+    <div>
+      <Charts option={getChartOption('')} />
+    </div>
+  );
 };
 export default ChartTest;
