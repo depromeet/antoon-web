d87acdff1fab4c1f68e8fb7b7b568c5e4c763234
tolluset <dlqud19@gmail.com>
2022-04-09 16:01:05 +0900

    chore: :art: modify folder naming to lower letter

--- pages/user/oauth/policy.tsx
+++ pages/user/oauth/policy.tsx
@@ -1,24 +1,24 @@
-import Checkbox from '../../../components/Policy/Checkbox';
+import Checkbox from '../../../components/policy/Checkbox';
 
 export const mock = [
-    {
-        id: 'servicePolicy',
-        link: 'https://www.notion.so/depromeet/bac78d41379c4c8181532b3e1412ef10',
-        label: '서비스 이용 약관',
-    },
-    {
-        id: 'privacyPolicy',
-        link: 'https://www.notion.so/depromeet/9b3b0e70be744bd889f154c06f7c2933',
-        label: '개인정보 수집 및 이용동의',
-    },
+  {
+    id: 'servicePolicy',
+    link: 'https://www.notion.so/depromeet/bac78d41379c4c8181532b3e1412ef10',
+    label: '서비스 이용 약관',
+  },
+  {
+    id: 'privacyPolicy',
+    link: 'https://www.notion.so/depromeet/9b3b0e70be744bd889f154c06f7c2933',
+    label: '개인정보 수집 및 이용동의',
+  },
 ];
 
 const policy = () => {
-    return mock.map((item) => (
-        <Checkbox key={item.id} id={item.id} link={item.link}>
-            {item.label}
-        </Checkbox>
-    ));
+  return mock.map((item) => (
+    <Checkbox key={item.id} id={item.id} link={item.link}>
+      {item.label}
+    </Checkbox>
+  ));
 };
 
 export default policy;
