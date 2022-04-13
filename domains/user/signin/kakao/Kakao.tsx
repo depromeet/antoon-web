7c91320ea38ea15d1cc46972fd2de7f317b4b281
김민지 <minzidev@gmail.com>
2022-04-13 15:03:27 +0900

    🔥 [chore] Next Auth 설정 제거 (#33)

--- domains/user/signin/kakao/Kakao.tsx
+++ domains/user/signin/kakao/Kakao.tsx
@@ -1,44 +1,7 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
-
-import {
-  KakaoWrap,
-  SignInBtn,
-  SignOutBtn,
-  User,
-  Name,
-  Email,
-  Img,
-} from './KaKao.style';
+import { KakaoWrap } from './KaKao.style';
 
 function Kakao() {
-  const { data: session, status } = useSession();
-
-  if (status === 'authenticated') {
-    return (
-      <KakaoWrap>
-        <SignOutBtn onClick={() => signOut()}>Logout</SignOutBtn>
-        <User>
-          <Name>{session.user.name}</Name>
-          <Email>{session.user.email}</Email>
-          <Img src={session.user.image} width="200px" height="200px" />
-        </User>
-      </KakaoWrap>
-    );
-  }
-
-  return (
-    <KakaoWrap>
-      <SignInBtn
-        onClick={() =>
-          signIn('kakao', {
-            callbackUrl: process.env.NEXTAUTH_URL,
-          })
-        }
-      >
-        Login
-      </SignInBtn>
-    </KakaoWrap>
-  );
+  return <KakaoWrap></KakaoWrap>;
 }
 
 export default Kakao;
