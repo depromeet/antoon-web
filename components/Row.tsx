5e69d154ffd999635c17d4c2dd912ac4e12c23b7
tolluset <dlqud19@gmail.com>
2022-04-21 22:19:59 +0900

    chore: :art: lint fix error

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,14 +1,13 @@
 import styled from '@emotion/styled';
 
 const Row = styled.div<any>`
-   {
-    display: 'flex';
-    flex-direction: 'row';
-    width: '100%';
-    justify-content: 'space-between';
-    ${(props) => props.centeredX && `justify-content: 'center'`};
-    ${(props) => props.centeredY && `align-items: 'center'`};
-  }
+  display: 'flex';
+  flex-direction: 'row';
+  justify-content: 'space-between';
+  ${(props) => props.centeredX && `justify-content: 'center'`};
+  ${(props) => props.centeredY && `align-items: 'center'`};
+
+  width: '100%';
 `;
 
 export default Row;
