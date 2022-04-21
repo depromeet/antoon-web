fb9459acad95f7c757c8eda4a91c2031cd6e96c6
tolluset <dlqud19@gmail.com>
2022-04-22 02:27:01 +0900

    feat: :lipstick: add bg color, font size themes

--- styles/themes/themes.ts
+++ styles/themes/themes.ts
@@ -1,4 +1,7 @@
 const themes = {
+  bgColor: {
+    primary: '#F1F1F1',
+  },
   colors: {
     black: 'rgba(0, 0, 0, 1)',
     black_700: '#191919',
@@ -7,6 +10,14 @@ const themes = {
     grey_100: 'rgb(245,256,256,1)',
     grey_200: '#f5f5f5',
     grey_300: '#cfcfcf',
+  },
+  padding: {
+    layout: '0 2.4rem',
+    layout_carousel: '0 0 0 2.4rem',
+  },
+  fontSize: {
+    h2: '2.4rem',
+    h3: '1.2rem',
   },
 };
 
