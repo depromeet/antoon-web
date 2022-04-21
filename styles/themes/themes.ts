149bd429eaf0822a0d3efbe3e59554feabdaa59b
tolluset <dlqud19@gmail.com>
2022-04-22 02:51:39 +0900

    feat: :lipstick: add gery 400 color theme

--- styles/themes/themes.ts
+++ styles/themes/themes.ts
@@ -1,3 +1,5 @@
+// TODO: 디자인 시스템에 맞춰 설정할 것
+// https://www.figma.com/file/fgeQYhg1cfGpb4sZe8cAi6/%EB%94%94%ED%94%84%EB%A7%8C-1%EB%B2%88-%EC%B6%9C%EA%B5%AC?node-id=3%3A17
 const themes = {
   bg_color: {
     primary: '#F1F1F1',
@@ -10,16 +12,11 @@ const themes = {
     grey_100: 'rgb(245,256,256,1)',
     grey_200: '#f5f5f5',
     grey_300: '#cfcfcf',
+    grey_400: '#A2A9B0',
   },
   padding: {
     layout: '0 2.4rem',
     layout_carousel: '0 0 0 2.4rem',
-  },
-  // TODO: 디자인 시스템에 맞춰 설정할 것
-  // https://www.figma.com/file/fgeQYhg1cfGpb4sZe8cAi6/%EB%94%94%ED%94%84%EB%A7%8C-1%EB%B2%88-%EC%B6%9C%EA%B5%AC?node-id=3%3A17
-  font_size: {
-    h2: '2.4rem',
-    h3: '1.2rem',
   },
 };
 
