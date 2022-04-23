d792f59fd7f80ce060cc6a0486eb8d188b0d4827
choipureum <pooreumsunny@gmail.com>
2022-04-23 18:08:07 +0900

    코드 정리

--- components/detail/tabs/Tabs.tsx
+++ components/detail/tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { TabsHeader, Tab, TabIndicator, TabsWrapper } from './Tabs.style';
 
 function Tabs() {
@@ -6,22 +6,10 @@ function Tabs() {
   const tabsHeaderRef = useRef<any>(null);
   const dayArr = ['1일', '1주', '1달', '3달'];
   const [currentTab, setCurrentTab] = useState(dayArr[0]);
-  const [tabsWidth, setTabsWidth] = useState(0);
-
-  useEffect(() => {
-    setTabsWidth(tabsHeaderRef.current.offsetWidth / 19);
-  }, []);
 
   const onClickHandler = (e: any) => {
     setCurrentTab(e.currentTarget.textContent);
     indicatorRef.current.style.left = `${e.currentTarget.offsetLeft + 4}px`;
-    console.log(e.currentTarget.offsetWidth);
-    /*
-    indicatorRef.current.style.left = `calc(calc(25%) * ${dayArr.indexOf(
-      e.currentTarget.textContent,
-    )} + 20px`;
-    */
-    //console.log(indicatorRef.current.style.left);
   };
 
   return (
