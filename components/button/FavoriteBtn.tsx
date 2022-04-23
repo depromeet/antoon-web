80e70b067a0b6b0c7dceb2a5c8b58ee2aaee8ffd
mnxmnz <codi0703@naver.com>
2022-04-23 20:16:36 +0900

    :sparkles: [feature] 좋아요 버튼 컴포넌트 생성

---
+++ components/button/FavoriteBtn.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useCallback } from 'react';
+
+import HeartIcon from '@components/icon/Icon_Heart';
+
+type FavoriteProps = {
+  isFavoriteChecked: boolean;
+};
+
+function FavoriteBtn({ isFavoriteChecked }: FavoriteProps) {
+  const [isLiked, setIsLiked] = useState(isFavoriteChecked);
+
+  const onClickFavorite = useCallback(() => {
+    setIsLiked((isLiked) => !isLiked);
+  }, []);
+
+  return <HeartIcon fill={isLiked} onClick={onClickFavorite} />;
+}
+
+export default FavoriteBtn;
