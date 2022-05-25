5539dfbbb2fb319a3f280b5cd75b736e43d99538
tolluset <dlqud19@gmail.com>
2022-05-25 23:32:07 +0900

    feat: :sparkles: add recommendation webtoon

--- domains/webtoon/home/Recommendation.style.tsx
+++ domains/webtoon/home/Recommendation.style.tsx
@@ -9,10 +9,55 @@ const RecommendationWrapper = styled.div`
 const CarouselBox = styled.div`
   display: flex;
   flex-direction: column;
-  border-radius: 20px;
-  background-color: ${(props) => props.theme.colors.primary.gray_300};
-  width: 200px;
-  height: 200px;
+  flex-direction: row;
+  border-radius: 2rem;
+  background-color: ${(props) => props.theme.colors.basic.white};
+  padding: 2rem;
+  width: 31.2rem;
+  height: 12rem;
 `;
 
-export { RecommendationWrapper, CarouselBox };
+const InformationWrapper = styled.div``;
+
+const GenreChip = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 4rem;
+  background-color: ${(props) => props.theme.colors.grayscale.gray_1000};
+  padding: 0.8rem;
+  width: fit-content;
+  height: 2rem;
+  color: ${(props) => props.theme.colors.basic.white};
+`;
+
+const Title = styled.div`
+  margin-right: 2.4rem;
+  padding-top: 1.8rem;
+  width: 16.8rem;
+  color: ${(props) => props.theme.colors.grayscale.gray_1000};
+  font-size: 1.6rem;
+  font-weight: bold;
+`;
+
+const RideHeadCount = styled.div`
+  padding-top: 0.6rem;
+  color: ${(props) => props.theme.colors.grayscale.gray_700};
+  font-size: 1.2rem;
+`;
+
+const WebtoonBox = styled.div`
+  border-radius: 0.8rem;
+  background-color: ${(props) => props.theme.colors.grayscale.gray_300};
+  width: 8rem;
+  height: 8rem;
+`;
+
+export {
+  RecommendationWrapper,
+  CarouselBox,
+  InformationWrapper,
+  GenreChip,
+  Title,
+  RideHeadCount,
+  WebtoonBox,
+};
