a50ac95c4e91c49d9c768952ceb412516bb34019
mnxmnz <codi0703@naver.com>
2022-05-26 14:29:50 +0900

    :sparkles: chore: notion 링크 수정

--- domains/user/mypage/MyPage.tsx
+++ domains/user/mypage/MyPage.tsx
@@ -26,7 +26,7 @@ function MyPage() {
         <CustomHr margin="2.4rem 0 1.6rem -2.4rem" />
         <SettingText>설정</SettingText>
         <PolicyLink
-          href="https://www.notion.so/ko-kr/product/"
+          href="https://antoon-fun.notion.site/5b10fdba48ff4d868f8d67ff4b894d34"
           target="_blank"
           rel="noreferrer noopener"
           margin="2.3rem 0 0"
@@ -35,7 +35,7 @@ function MyPage() {
           <ChebronRightIcon />
         </PolicyLink>
         <PolicyLink
-          href="https://www.notion.so/ko-kr/product/"
+          href="https://antoon-fun.notion.site/2693f5030fb24a4c9bd1c9ff958c680e"
           target="_blank"
           rel="noreferrer noopener"
           margin="3.1rem 0 1.6rem"
