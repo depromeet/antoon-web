25b7bd43b9f2e7bf2278f3665d4011b0beecafdb
choipureum <pooreumsunny@gmail.com>
2022-05-29 03:42:35 +0900

    :fire:[feature] Add Detail DataFetching
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

--- types/webtoon-type.d.ts
+++ types/webtoon-type.d.ts
@@ -7,7 +7,25 @@ export enum PlatformEnum {
   LEZHIN = '레진코믹스',
   ETC = '기타',
 }
-
+/*
+export interface Webtoon {
+  webtoondId: number;
+  title: string;
+  content: string;
+  webtoonUrl: string;
+  thumbnail: string;
+  platform: PlatformType;
+  platformDescription: string;
+  status: PUBLISH;
+  statusDescription: string;
+  genres: WebtoonCategory[];
+  publishDays: WebtoonPublishDays[];
+  writers: WebtoonWriter[];
+  recommendationCountId: number;
+  joinCount: number;
+  leaveCount: number;
+}
+*/
 export interface Webtoon {
   webtoondId: number;
   title: string;
@@ -27,6 +45,17 @@ export interface Webtoon {
 }
 
 export type ChartStatus = 'UP' | 'DOWN' | 'STALE';
+
+export interface WebtoonCategory {
+  webtoonGenreId: number;
+  genreCategory: string;
+  genreCategoryDescription: Genre;
+}
+
+export interface WebtoonPublishDays {
+  webtoonPublishDayId: number;
+  day: string;
+}
 
 export interface WebtoonCategory {
   webtoonGenreId: number;
