56de86d5be39377718b5e5500b6356cb7e7bdfed
mnxmnz <codi0703@naver.com>
2022-05-29 17:46:23 +0900

    :sparkles: feat: 검색 자동 완성 페이지 이동 기능 추가

--- domains/search/AutoCompletList.tsx
+++ domains/search/AutoCompletList.tsx
@@ -1,14 +1,44 @@
 import SearchIcon from '@assets/icons/SearchIcon';
+import { useRouter } from 'next/router';
+
+import { api } from '@apis/api';
 
 import { AutoCompleteListWrap, AutoCompleteList } from './AutoComplet.style';
 
 function AutoCompletList({
   autoCompleteList,
+  setSearchResults,
+  setSearchInput,
 }: {
   autoCompleteList: { id: number; title: string };
+  setSearchResults: any;
+  setSearchInput: any;
 }) {
+  const router = useRouter();
+
+  const onClickAutoCompleteList = async () => {
+    try {
+      const keywordResults = await api
+        .post(`webtoons/search`, {
+          json: {
+            webtoons: [autoCompleteList.id],
+          },
+        })
+        .json();
+
+      setSearchInput(autoCompleteList.title);
+      setSearchResults(keywordResults as any);
+      router.push(`/search?keyword=${autoCompleteList.title}`);
+    } catch (e) {
+      return e;
+    }
+  };
+
   return (
-    <AutoCompleteListWrap key={autoCompleteList.id}>
+    <AutoCompleteListWrap
+      key={autoCompleteList.id}
+      onClick={onClickAutoCompleteList}
+    >
       <SearchIcon stroke="#B6BCC7" />
       <AutoCompleteList>{autoCompleteList.title}</AutoCompleteList>
     </AutoCompleteListWrap>
