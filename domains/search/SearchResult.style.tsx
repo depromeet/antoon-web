858d1053865d1cc60c30d92d4a8a55f674e7a9a1
mnxmnz <codi0703@naver.com>
2022-05-29 17:49:05 +0900

    :lipstick: chore: 스코어 컴포넌트 생성

--- domains/search/SearchResult.style.tsx
+++ domains/search/SearchResult.style.tsx
@@ -1,15 +1,33 @@
 import styled from '@emotion/styled';
 import Image from 'next/image';
 
+type ImgProps = {
+  width: string;
+  height: string;
+};
+
 const SearchResultWrap = styled.div`
   display: flex;
   align-items: center;
   margin: 1.5rem 0 0;
+  cursor: pointer;
   width: 100%;
+`;
+
+const ThumbnailWrap = styled.div<ImgProps>`
+  position: relative;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
 `;
 
 const Thumbnail = styled(Image)`
   border-radius: 1.6rem;
+`;
+
+const WebtoonInfoWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 const MainInfoWrap = styled.div`
@@ -22,9 +40,45 @@ const Title = styled.p`
   font-weight: 600;
 `;
 
+const WriterWrap = styled.div`
+  display: flex;
+`;
+
 const Writer = styled.p`
+  margin: 0 0.5rem 0 0;
   color: ${(props) => props.theme.colors.grayscale.gray_600};
   font-size: 1.4rem;
 `;
 
-export { SearchResultWrap, Thumbnail, MainInfoWrap, Title, Writer };
+const ScoreInfoWrap = styled.div`
+  text-align: right;
+`;
+
+const Score = styled.div`
+  margin: 0 0 0.5rem;
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+const Percent = styled.div`
+  border-radius: 0.4rem;
+  background-color: ${(props) => props.theme.colors.grayscale.gray_200};
+  padding: 0.1rem 0.4rem;
+  color: ${(props) => props.theme.colors.grayscale.gray_700};
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
+
+export {
+  SearchResultWrap,
+  ThumbnailWrap,
+  Thumbnail,
+  WebtoonInfoWrap,
+  MainInfoWrap,
+  Title,
+  WriterWrap,
+  Writer,
+  ScoreInfoWrap,
+  Score,
+  Percent,
+};
