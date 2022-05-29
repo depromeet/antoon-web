e8a1fa4193a65230c86f2de4fea66414b2cabd78
mnxmnz <codi0703@naver.com>
2022-05-29 17:50:12 +0900

    :sparkles: feat: 검색 결과 링크 이동 기능 추가

--- domains/search/SearchResult.tsx
+++ domains/search/SearchResult.tsx
@@ -1,36 +1,65 @@
+import { useRouter } from 'next/router';
+
 import deafultImage from '@components/image/default';
 
 import {
   SearchResultWrap,
+  ThumbnailWrap,
   Thumbnail,
+  WebtoonInfoWrap,
   MainInfoWrap,
   Title,
+  WriterWrap,
   Writer,
+  ScoreInfoWrap,
+  Score,
+  Percent,
 } from './SearchResult.style';
 
 import { WebtoonSearch } from '@_types/webtoon-type';
 
 function SearchResult({ searchResults }: { searchResults: WebtoonSearch }) {
+  const router = useRouter();
+
   return (
-    <SearchResultWrap key={searchResults.id}>
-      <Thumbnail
-        alt={searchResults.title}
-        src={searchResults.thumbnail || deafultImage}
-        placeholder="blur"
-        blurDataURL={searchResults.thumbnail || deafultImage}
-        width={64}
-        height={64}
-        layout="fixed"
-        objectFit="cover"
-      />
-      <MainInfoWrap>
-        <Title>{searchResults.title}</Title>
-        {searchResults.writers.map(
-          (writer: { webtoonWriterId: number; name: string }) => {
-            return <Writer key={writer.webtoonWriterId}>{writer.name}</Writer>;
-          },
-        )}
-      </MainInfoWrap>
+    <SearchResultWrap
+      key={searchResults.webtoonId}
+      onClick={() => {
+        router.push(`/webtoon/${searchResults.webtoonId}`);
+      }}
+    >
+      <ThumbnailWrap width="64" height="64">
+        <Thumbnail
+          alt={searchResults.title}
+          src={searchResults.thumbnail || deafultImage}
+          placeholder="blur"
+          blurDataURL={searchResults.thumbnail || deafultImage}
+          width={64}
+          height={64}
+          layout="fixed"
+          objectFit="cover"
+        />
+      </ThumbnailWrap>
+
+      <WebtoonInfoWrap>
+        <MainInfoWrap>
+          <Title>{searchResults.title}</Title>
+          <WriterWrap>
+            {searchResults.writers.map(
+              (writer: { webtoonWriterId: number; name: string }) => {
+                return (
+                  <Writer key={writer.webtoonWriterId}>{writer.name}</Writer>
+                );
+              },
+            )}
+          </WriterWrap>
+        </MainInfoWrap>
+
+        <ScoreInfoWrap>
+          <Score>{searchResults.score}</Score>
+          <Percent>{searchResults.scoreGapPercent}.00%</Percent>
+        </ScoreInfoWrap>
+      </WebtoonInfoWrap>
     </SearchResultWrap>
   );
 }
