f0f8252f377dd3d2f7e7f120199542f2b2432877
mnxmnz <codi0703@naver.com>
2022-06-01 20:06:53 +0900

    :sparkles: feat: 사용자 정보, 토큰 API 생성

---
+++ apis/user.ts
@@ -0,0 +1,37 @@
+import { useQuery } from 'react-query';
+import { user } from './queryKeys';
+import { api } from './api';
+
+const getToken = async (refreshToken: string) => {
+  return await api
+    .post(
+      'auth/refresh',
+      {},
+      {
+        headers: {
+          Refresh: refreshToken,
+        },
+      },
+    )
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+};
+
+const useGetToken = (refreshToken: string) => {
+  return useQuery(user.tokens(refreshToken), () => getToken(refreshToken), {
+    refetchOnMount: 'always',
+  });
+};
+
+const getUserInformation = async () => {
+  return await api
+    .get('users')
+    .then((res) => res.data)
+    .catch((e) => console.log(e));
+};
+
+const useGetUserInformation = () => {
+  return useQuery(user.information(), () => getUserInformation());
+};
+
+export { getUserInformation, useGetUserInformation, getToken, useGetToken };
