8191c106f17e232122d620319b703f29f592497c
mnxmnz <codi0703@naver.com>
2022-06-01 10:39:42 +0900

    :fire: chore: 임시로 헤더 설정 변경

--- components/layout/Header/HeaderRightMenu.tsx
+++ components/layout/Header/HeaderRightMenu.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { getCookie } from 'cookies-next';
 
 import SearchIcon from '@assets/icons/SearchIcon';
 import ShareIcon from '@assets/icons/ShareIcon';
@@ -8,13 +7,9 @@ import UserProfile from '@components/image/UserProfile';
 
 import { RightWrap, MenuWrap } from './HeaderRightMenu.style';
 
-import { comments } from '@domains/webtoon/detail/Comment.data';
 import ProfileDefaultImg from '@assets/images/ProfileDefaultImg';
 
 function HeaderRightMenu({ rightBtn }: { rightBtn?: string }) {
-  const data = comments;
-  const token = getCookie('access');
-
   return (
     <RightWrap>
       {rightBtn === 'menu' ? (
@@ -24,7 +19,7 @@ function HeaderRightMenu({ rightBtn }: { rightBtn?: string }) {
               <SearchIcon />
             </a>
           </Link>
-          {token === undefined ? (
+          {/* {token === undefined ? (
             <Link href="/user/signin" passHref>
               <a>
                 <ProfileDefaultImg width="24" height="24" />
@@ -33,10 +28,16 @@ function HeaderRightMenu({ rightBtn }: { rightBtn?: string }) {
           ) : (
             <Link href="/user/mypage" passHref>
               <a>
-                <UserProfile src={data[0].profileimg} width="24" height="24" />
+                <UserProfile
+                  src={
+                    'https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+                  }
+                  width="24"
+                  height="24"
+                />
               </a>
             </Link>
-          )}
+          )} */}
         </MenuWrap>
       ) : rightBtn === 'share' ? (
         <ShareIcon />
