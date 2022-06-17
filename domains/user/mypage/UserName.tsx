f0a79b7d39e5643a8b205208cc1393f63f32545a
mnxmnz <codi0703@naver.com>
2022-06-17 20:29:20 +0900

    :lipstick: chore: 사용자 이름 컴포넌트 생성

--- domains/user/mypage/UserName.tsx
+++ domains/user/mypage/UserName.tsx
@@ -1,7 +1,15 @@
-import { UserNameWrap } from './UserName.style';
+import ProfileNameEdit from '@assets/icons/ProfileNameEdit';
+import { UserNameWrap, Name } from './UserName.style';
 
 function UserName({ name }: { name: string }) {
-  return <UserNameWrap>{name}</UserNameWrap>;
+  return (
+    <>
+      <UserNameWrap>
+        <Name>{name}</Name>
+        <ProfileNameEdit />
+      </UserNameWrap>
+    </>
+  );
 }
 
 export default UserName;
