888d81a86f81c9447fc653c9624d2bc07a6bf7f1
choipureum <pooreumsunny@gmail.com>
2022-06-21 23:42:15 +0900

    :fire:[feature]: 투표 A/B, 초이스 결과 마크업 완료
    Signed-off-by: choipureum <pooreumsunny@gmail.com>

--- components/vote/ABVoteItem.style.tsx
+++ components/vote/ABVoteItem.style.tsx
@@ -1,15 +1,22 @@
 import styled from '@emotion/styled';
 
-const ABVoteItemWrapper = styled.div`
+const ABVoteItemWrapper = styled.div<{ isWinner: boolean }>`
   position: relative;
   margin-top: 20px;
   border-radius: 20px;
-  background-color: ${(props) => props.theme.colors.grayscale.gray_200};
+  background-color: ${(props) =>
+    props.isWinner
+      ? props.theme.colors.chart.point_up_100
+      : props.theme.colors.grayscale.gray_200};
   width: 100%;
   height: 100%;
 `;
 
-const ABVoteFilter = styled.div`
+const ABVoteFilter = styled.div<{
+  isEnd: boolean;
+  isWinner: boolean;
+  dataWidth: number;
+}>`
   position: absolute;
   top: 0;
   right: 0;
@@ -17,13 +24,22 @@ const ABVoteFilter = styled.div`
   left: 0;
   opacity: 0.5;
   z-index: 2;
-  border-radius: 20px;
+  border-radius: ${(props) =>
+    props.dataWidth >= 90
+      ? '20px 20px 0 0'
+      : props.isEnd
+      ? '20px 0 0 0'
+      : '20px'};
   background-color: ${(props) => props.theme.colors.chart.point_up_100};
   cursor: pointer;
-  width: 100%;
-  height: 100%;
+  width: ${(props) => props.dataWidth}%;
+  height: ${(props) => (props.isEnd ? '160px' : '100%;')};
   text-align: center;
   -webkit-tap-highlight-color: transparent;
+`;
+const ABVoteWinnerMark = styled.span`
+  position: absolute;
+  bottom: 100px;
 `;
 
 const ABVoteImg = styled.div<{ imageSrc: string }>`
@@ -32,9 +48,11 @@ const ABVoteImg = styled.div<{ imageSrc: string }>`
   background-size: cover;
   height: 160px;
 `;
-const ABVoteContentWrapper = styled.div`
+const ABVoteContentWrapper = styled.div<{ isEnd: boolean; isWinner: boolean }>`
+  z-index: ${(props) => (props.isEnd ? 3 : 0)};
   padding: 15px;
   height: 50px;
+  color: ${(props) => (props.isWinner ? props.theme.colors.basic.white : '')};
   font-size: 12px;
 `;
 
@@ -56,11 +74,51 @@ const ABVoteContent = styled.span`
   font-weight: 500;
 `;
 
+const ABVoteResult = styled.div<{ isEnd: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 7%;
+  z-index: ${(props) => (props.isEnd ? 3 : 0)};
+  line-height: 11.4;
+`;
+
+const ABVoteResultPercent = styled.span<{ isWinner: boolean }>`
+  margin-right: 10px;
+  color: ${(props) => props.theme.colors.basic.white};
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const ABVoteResultVoter = styled.span`
+  color: ${(props) => props.theme.colors.basic.white};
+  font-size: 13px;
+  font-weight: 300;
+`;
+
+const ABWinnerType = styled.span`
+  display: inline-block;
+  margin-right: 10px;
+  border-radius: 50%;
+  background: ${(props) => props.theme.colors.basic.white};
+  padding-top: 3px;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  color: ${(props) => props.theme.colors.chart.point_up_100};
+  font-size: 13px;
+  font-weight: 500;
+`;
+
 export {
   ABVoteItemWrapper,
   ABVoteImg,
+  ABVoteWinnerMark,
   ABVoteFilter,
   ABVoteContentWrapper,
   ABVoteType,
   ABVoteContent,
+  ABVoteResult,
+  ABVoteResultPercent,
+  ABVoteResultVoter,
+  ABWinnerType,
 };
