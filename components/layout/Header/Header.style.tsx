6c695d79ac5d025ed7e8067330469babe4485bc4
mnxmnz <codi0703@naver.com>
2022-06-25 17:49:54 +0900

    :lipstick: fix: 헤더 width 에러 해결

--- components/layout/Header/Header.style.tsx
+++ components/layout/Header/Header.style.tsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled';
 
-const HeaderWrap = styled.header`
+const HeaderWrap = styled.header<{ headerColor: string }>`
   display: flex;
   position: sticky;
   top: 0;
   align-items: center;
   z-index: 100;
-  background-color: ${(props) => props.theme.colors.basic.white};
-  width: 100%;
+  margin: 0 0 0 -2.4rem;
+  background-color: ${(props) =>
+    props.headerColor ? props.headerColor : props.theme.colors.basic.white};
+  padding: 0 2.4rem;
+  width: calc(100% + 4.8rem);
   height: 5.6rem;
 `;
 
