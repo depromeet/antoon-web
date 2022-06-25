bcd64751110a8a55168bf386ccc84f6781969962
mnxmnz <codi0703@naver.com>
2022-06-25 17:54:20 +0900

    :lipstick: feat: 2차 MVP 로그인 페이지

--- domains/user/signin/SignInMain.style.tsx
+++ domains/user/signin/SignInMain.style.tsx
@@ -2,17 +2,28 @@ import styled from '@emotion/styled';
 
 const SignInWrap = styled.div`
   display: flex;
+  position: relative;
   flex-direction: column;
   align-items: center;
+  margin: 0 0 0 -2.4rem;
+  background-color: ${(props) => props.theme.colors.grayscale.gray_1000};
+  background-image: url('/images/loginpage_bg.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: calc(100% + 4.8rem);
+  height: calc(100vh - 5.6rem);
+  background-position-y: 15rem;
 `;
 
-const GoToMain = styled.button`
-  display: flex;
-  align-items: center;
-  margin: 1.65rem 0;
-  color: ${(props) => props.theme.colors.grayscale.gray_600};
-  font-size: 1.4rem;
-  font-weight: 600;
+const BtnWrap = styled.div`
+  position: absolute;
+  bottom: 10%;
+  margin: 0 0 1.2rem;
+  text-align: center;
+
+  button:nth-of-type(1) {
+    margin: 0 0 1.2rem;
+  }
 `;
 
-export { SignInWrap, GoToMain };
+export { SignInWrap, BtnWrap };
