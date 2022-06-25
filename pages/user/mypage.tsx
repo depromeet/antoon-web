07589750548d4d0d40306bae8cc7f94c5bb411f2
mnxmnz <codi0703@naver.com>
2022-06-25 10:01:01 +0900

    :sparkles: feat: 로그인 마이페이지, 비로그인 마이페이지 설정

--- pages/user/mypage.tsx
+++ pages/user/mypage.tsx
@@ -1,8 +1,15 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { getCookie } from 'cookies-next';
 import { Mixpanel } from 'mixpanel';
 
 import Header from '@components/layout/Header/Header';
 import MyPageWrap from '@domains/user/mypage/MyPage';
+import DefaultMyPage from '@domains/user/mypage/DefaultMyPage';
+import MyPageMenu from '@domains/user/mypage/MyPageMenu';
+
+import { Menu } from '@domains/user/mypage/MyPage.style';
+import SignoutBtn from '@domains/user/mypage/SignoutBtn';
+import SigninBtn from '@domains/user/mypage/SigninBtn';
 
 function MyPage() {
   useEffect(() => {
@@ -11,10 +18,26 @@ function MyPage() {
     });
   }, []);
 
+  const accessToken = getCookie('Access');
+
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
   return (
     <>
-      <Header title="마이페이지" />
-      <MyPageWrap />
+      {!isSSR && (
+        <>
+          <Header title="마이페이지" />
+          {accessToken ? <MyPageWrap /> : <DefaultMyPage />}
+          <Menu>
+            <MyPageMenu />
+            {accessToken ? <SignoutBtn /> : <SigninBtn />}
+          </Menu>
+        </>
+      )}
     </>
   );
 }
