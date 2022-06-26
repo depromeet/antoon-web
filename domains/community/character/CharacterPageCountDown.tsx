ddad20474c7cefe834701009bb3ae5fee67b1d9a
mnxmnz <codi0703@naver.com>
2022-06-27 05:48:17 +0900

    :sparkles: feat: 인물, 커플 상세 페이지

---
+++ domains/community/character/CharacterPageCountDown.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+import useCountdown from '@hooks/useCountdown';
+
+import { TimerWrap } from './CharacterPageCountDown.style';
+
+function CharacterPageCountDown({ endTime }: { endTime: string }) {
+  const [hours, minutes, seconds] = useCountdown(endTime);
+
+  const [isHide, setIsHide] = useState(false);
+
+  useEffect(() => {
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      setIsHide(true);
+    } else {
+      setIsHide(false);
+    }
+  }, [hours, minutes, seconds]);
+
+  return (
+    <TimerWrap>
+      {!isHide
+        ? `투표 종료까지 ${hours}시간 : ${minutes}분 : ${seconds}초 남음`
+        : '투표 종료'}
+    </TimerWrap>
+  );
+}
+
+export default CharacterPageCountDown;
