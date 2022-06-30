5c0f28519b029faad735f3106b638a53f684c188
Lee Byonghun <dlqud19@gmail.com>
2022-07-01 01:49:59 +0900

    [온보딩] mvp2차 온보딩 추가 (#175)

--- components/modal/onboard/Modal.tsx
+++ components/modal/onboard/Modal.tsx
@@ -21,7 +21,7 @@ import {
 
 createPortal;
 
-type Step = 'first' | 'second' | 'third';
+type Step = 'first' | 'second' | 'third' | 'fourth' | 'fifth';
 
 function Modal() {
   const [modalOpen, setModalOpen] = useState(true);
@@ -90,9 +90,8 @@ function Modal() {
           웹툰 차트에 반영돼요!
         </DescriptionMain>
         <DescriptionDetail>
-          재미있는 웹툰에는 탑승!
-          <br />
-          아쉬운 웹툰에는 하차!
+          한 웹툰의 탑승/하차는 하루에 한번 가능!
+          <br />낮 12시에 투표가 열려요.
         </DescriptionDetail>
       </DescriptionWrapper>
       <ButtonContainer>
@@ -105,6 +104,12 @@ function Modal() {
             <StepIndicator />
           </IndicatorWrapper>
           <IndicatorWrapper onClick={() => onStep('third')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fourth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fifth')}>
             <StepIndicator />
           </IndicatorWrapper>
         </IndicatorContainer>
@@ -125,26 +130,32 @@ function Modal() {
       </ImageWrapper>
       <DescriptionWrapper>
         <DescriptionMain>
-          투표는 하루에 한번
+          좋아하는 인물과 커플 주식에
           <br />
-          가능해요.
+          탑승할 수 있어요!
         </DescriptionMain>
         <DescriptionDetail>
-          매일 낮 12시에 투표가 갱신돼요.
+          댓글에만 존재했던 등장인물 주식,
           <br />
-          개미의 한 표로 차트를 움직여봐요!
+          안트코인으로 직접 탑승해보세요!
         </DescriptionDetail>
       </DescriptionWrapper>
       <ButtonContainer>
         <PrevButton onClick={() => onStep('first')}>이전</PrevButton>
         <IndicatorContainer>
-          <IndicatorWrapper onClick={() => onStep('first')}>
+          <IndicatorWrapper onClick={() => onStep('first')} currentStep={true}>
             <StepIndicator />
           </IndicatorWrapper>
-          <IndicatorWrapper onClick={() => onStep('second')} currentStep={true}>
+          <IndicatorWrapper onClick={() => onStep('second')}>
             <StepIndicator active={true} />
           </IndicatorWrapper>
           <IndicatorWrapper onClick={() => onStep('third')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fourth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fifth')}>
             <StepIndicator />
           </IndicatorWrapper>
         </IndicatorContainer>
@@ -165,26 +176,124 @@ function Modal() {
       </ImageWrapper>
       <DescriptionWrapper>
         <DescriptionMain>
-          개미들의
+          안트코인을 사용해
           <br />
-          행진에 동참해보세요!
+          관심있는 주제에 투표하세요!
         </DescriptionMain>
         <DescriptionDetail>
-          댓글을 통해 개미들과
+          댓글로만 소통했던 웹툰 떡밥,
           <br />
-          아열띤 토론을 나눠보세요!
+          이제는 A/B, 초이스로 해봐요!
         </DescriptionDetail>
       </DescriptionWrapper>
       <ButtonContainer>
         <PrevButton onClick={() => onStep('second')}>이전</PrevButton>
         <IndicatorContainer>
-          <IndicatorWrapper onClick={() => onStep('first')}>
+          <IndicatorWrapper onClick={() => onStep('first')} currentStep={true}>
             <StepIndicator />
           </IndicatorWrapper>
           <IndicatorWrapper onClick={() => onStep('second')}>
             <StepIndicator />
           </IndicatorWrapper>
-          <IndicatorWrapper onClick={() => onStep('third')} currentStep={true}>
+          <IndicatorWrapper onClick={() => onStep('third')}>
+            <StepIndicator active={true} />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fourth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fifth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+        </IndicatorContainer>
+        <NextButton onClick={() => onStep('fourth')}>다음</NextButton>
+      </ButtonContainer>
+    </>
+  );
+
+  const Fourth = (
+    <>
+      <ImageWrapper>
+        <Image
+          src="/images/onboarding_introduce_04.png"
+          alt="개미들의 행진에 동참해보세요!"
+          width={312}
+          height={200}
+        />
+      </ImageWrapper>
+      <DescriptionWrapper>
+        <DescriptionMain>
+          안트코인은
+          <br />
+          쉽게 모을 수 있어요!
+        </DescriptionMain>
+        <DescriptionDetail>
+          웹툰 탑승/하차와 참여한 투표에
+          <br />
+          이기면 안트코인을 지급해줘요!
+        </DescriptionDetail>
+      </DescriptionWrapper>
+      <ButtonContainer>
+        <PrevButton onClick={() => onStep('third')}>이전</PrevButton>
+        <IndicatorContainer>
+          <IndicatorWrapper onClick={() => onStep('first')} currentStep={true}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('second')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('third')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fourth')}>
+            <StepIndicator active={true} />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fifth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+        </IndicatorContainer>
+        <NextButton onClick={() => onStep('fifth')}>다음</NextButton>
+      </ButtonContainer>
+    </>
+  );
+
+  const Fifth = (
+    <>
+      <ImageWrapper>
+        <Image
+          src="/images/onboarding_introduce_05.png"
+          alt="개미들의 행진에 동참해보세요!"
+          width={312}
+          height={200}
+        />
+      </ImageWrapper>
+      <DescriptionWrapper>
+        <DescriptionMain>
+          모든 웹툰의 떡밥을
+          <br />
+          개미는 툰툰에서!
+        </DescriptionMain>
+        <DescriptionDetail>
+          댓글을 통해 개미들과
+          <br />
+          열띤 토론을 나눠보세요!
+        </DescriptionDetail>
+      </DescriptionWrapper>
+      <ButtonContainer>
+        <PrevButton onClick={() => onStep('fourth')}>이전</PrevButton>
+        <IndicatorContainer>
+          <IndicatorWrapper onClick={() => onStep('first')} currentStep={true}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('second')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('third')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fourth')}>
+            <StepIndicator />
+          </IndicatorWrapper>
+          <IndicatorWrapper onClick={() => onStep('fifth')}>
             <StepIndicator active={true} />
           </IndicatorWrapper>
         </IndicatorContainer>
@@ -201,6 +310,10 @@ function Modal() {
         return Second;
       case 'third':
         return Third;
+      case 'fourth':
+        return Fourth;
+      case 'fifth':
+        return Fifth;
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [step]);
