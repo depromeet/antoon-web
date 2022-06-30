770584749de0c643e642b37e9959eb776712622a
mnxmnz <codi0703@naver.com>
2022-06-30 15:20:19 +0900

    :lipstick: chore: background color qa 반영

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -15,7 +15,7 @@ const global = css`
   }
 
   html {
-    background-color: #eceff4;
+    background-color: #d9dee5;
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
       Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo',
       'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji',
