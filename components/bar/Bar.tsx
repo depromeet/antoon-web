2c6ff7f635664cd62063532a8c66e6a3cc6747cc
mnxmnz <codi0703@naver.com>
2022-07-01 13:43:48 +0900

    :lipstick: chore: 웹툰 상세 페이지 구분선 수정

--- components/bar/Bar.tsx
+++ components/bar/Bar.tsx
@@ -2,9 +2,10 @@ import styled from '@emotion/styled';
 
 const Bar = styled.div`
   position: absolute;
-  left: 0;
-  border-bottom: 8px solid ${(props) => props.theme.colors.grayscale.gray_300};
-  width: 100%;
+  margin: 0 0 0 -24px;
+  border-bottom: 8px solid ${(props) => props.theme.colors.grayscale.gray_100};
+  width: min(480px, 100%);
+  text-align: center;
 `;
 
 export default Bar;
