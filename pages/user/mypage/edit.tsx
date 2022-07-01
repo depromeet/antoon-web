eb8683420dbd459bc4a25f6be1836b052295f21e
mnxmnz <codi0703@naver.com>
2022-07-02 02:13:09 +0900

    :fire: fix: remove profile img edit feature

--- pages/user/mypage/edit.tsx
+++ pages/user/mypage/edit.tsx
@@ -3,12 +3,14 @@ import { useRouter } from 'next/router';
 import { getCookie } from 'cookies-next';
 import { Mixpanel } from 'mixpanel';
 
+import ProfileDefaultImg from '@assets/images/ProfileDefaultImg';
+
 import { useGetUserInformation } from '@apis/user';
 
 import Header from '@components/layout/Header/Header';
 import OnError from '@components/OnError';
+import UserProfile from '@components/image/UserProfile';
 import EditName from '@domains/user/mypage/edit/EditName';
-import EditImg from '@domains/user/mypage/edit/EditImg';
 
 function Edit() {
   const router = useRouter();
@@ -36,7 +38,11 @@ function Edit() {
         headerTitle="프로필 수정"
         headerRight="없음"
       />
-      <EditImg user={user} />
+      {user ? (
+        <UserProfile src={user.imageUrl} width="100" height="100" />
+      ) : (
+        <ProfileDefaultImg width="100" height="100" />
+      )}
       <EditName user={user} />
     </>
   );
