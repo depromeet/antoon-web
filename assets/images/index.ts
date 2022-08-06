3764c794f2af3d562984e2d352253a22e9bb6653
tolluset <dlqud19@gmail.com>
2022-08-07 03:44:07 +0900

    :lipstick: refactor: add on-board images to svgs and save in bundle

---
+++ assets/images/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as OnBoardingIntroduce01 } from './onboarding_introduce_01.svg';
+export { ReactComponent as OnBoardingIntroduce02 } from './onboarding_introduce_02.svg';
+export { ReactComponent as OnBoardingIntroduce03 } from './onboarding_introduce_03.svg';
+export { ReactComponent as OnBoardingIntroduce04 } from './onboarding_introduce_04.svg';
+export { ReactComponent as OnBoardingIntroduce05 } from './onboarding_introduce_05.svg';
