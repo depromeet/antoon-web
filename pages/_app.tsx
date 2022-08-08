f23948ec4a0c4ee787117d3f28fa7d5a90500998
mnxmnz <codi0703@naver.com>
2022-08-08 19:57:06 +0900

    :pencil2: chore: meta description 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,14 +35,8 @@ function MyApp({ Component, pageProps }: any) {
         <meta property="og:site_name" content="개미는 툰툰" />
         <meta property="og:title" content="개미는 툰툰" />
         <meta name="twitter:title" content="개미는 툰툰" />
-        <meta
-          property="og:description"
-          content="독자들의 참여로 이루어지는 웹툰 콘텐츠 추천 및 커뮤니티 서비스"
-        />
-        <meta
-          name="twitter:description"
-          content="독자들의 참여로 이루어지는 웹툰 콘텐츠 추천 및 커뮤니티 서비스"
-        />
+        <meta property="og:description" content="웹툰의 새로운 덕질 문화" />
+        <meta name="twitter:description" content="웹툰의 새로운 덕질 문화" />
         <meta
           property="og:image"
           content="https://camo.githubusercontent.com/d9f6f82f2ccdd9692d7d2b275a0d35fcd9aba3fefb52777bc0a88baaea670fed/68747470733a2f2f696d67312e6461756d63646e2e6e65742f7468756d622f523132383078302f3f73636f64653d6d746973746f72793226666e616d653d6874747073253341253246253246626c6f672e6b616b616f63646e2e6e6574253246646e253246466137384f253246627472436a32556f56424f2532465a55486b366b6a4e4146516f613670614f3341476c31253246696d672e706e67"
