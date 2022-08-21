bedf03e7dd4769729a3fbcdcebec2823fea9424f
mnxmnz <codi0703@naver.com>
2022-08-22 05:11:21 +0900

    :sparkles: feature: 페이지 이동 router 설정

--- domains/user/mypage/coin/Coin.tsx
+++ domains/user/mypage/coin/Coin.tsx
@@ -1,20 +1,30 @@
-import { MyPageCoin } from '@assets/icons';
+import { useRouter } from 'next/router';
+
+import { MyPageCoin, ChebronRightWhite } from '@assets/icons';
 
 import {
   CoinWrap,
   CoinAmount,
   Coins,
   Text,
+  ButtonWrap,
   TextGuide,
 } from '@domains/user/mypage/coin/Coin.style';
 
 function Coin({ coin }: { coin: number }) {
+  const router = useRouter();
+
   return (
     <CoinWrap>
       <div>
-        <CoinAmount>
+        <CoinAmount onClick={() => router.push('/user/mypage/coins')}>
           <Coins>{coin}</Coins>
           <Text>ANT</Text>
+          {0 < coin && (
+            <ButtonWrap>
+              <ChebronRightWhite />
+            </ButtonWrap>
+          )}
         </CoinAmount>
         <TextGuide>보유 안트코인</TextGuide>
       </div>
